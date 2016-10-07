f0b82c0c4d21c60802625288f6621f1cff082733
Leconize <g.supavit@gmail.com>
2016-10-07 20:26:34 +0700

    Commit before Merge

--- app/src/main/java/com/example/supphawit/friend_trip/activity/MainActivity.java
+++ app/src/main/java/com/example/supphawit/friend_trip/activity/MainActivity.java
@@ -3,23 +3,43 @@ package com.example.supphawit.friend_trip.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 import com.example.supphawit.friend_trip.R;
+import com.example.supphawit.friend_trip.activity.model.Trip;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseDatabase database;
-    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, CreateTripActivity.class);
+        DatabaseReference test = FirebaseDatabase.getInstance().getReference("trips");
+        test.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot child: dataSnapshot.getChildren()){
+                    Trip trip = child.getValue(Trip.class);
+                    Log.d("Trip", trip.getPlaces());
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
